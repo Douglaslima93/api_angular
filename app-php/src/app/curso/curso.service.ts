624dd1a9235f679f9311ea2c120e263a374ba169
Douglas Lima <douglas07a@gmail.com>
2024-01-08 11:56:15 -0300

    Novo serviço remover

--- app-php/src/app/curso/curso.service.ts
+++ app-php/src/app/curso/curso.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Curso } from './curso';
@@ -34,6 +34,23 @@ export class CursoService {
     .pipe(map((res:any) => {
       this.vetor.push(res['cursos']);
       return this.vetor;
+    }))
+  }
+
+  //Remover Curso
+  removerCurso(c: Curso): Observable<Curso[]>{
+
+    const params = new HttpParams().set("idCurso", c.idCurso.toString());
+
+    return this.http.delete(this.url+'excluir', {params: params})
+    .pipe(map((res) => {
+
+      const filtro = this.vetor.filter((curso) => {
+        return +curso['idCurso'] !== +c.idCurso;
+      });
+
+      return this.vetor = filtro;
+
     }))
   }
 }
